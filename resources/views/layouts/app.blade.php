09076bde77061edc53213e762dd566b73a3cdb40
Fabian <footballstudios4k@gmail.com>
2025-04-03 14:00:47 -0500

    V3.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <script src="{{ asset('resources\js\app.js') }}"></script>
-    @vite(['resources/js/app.js'])
     <!-- Agrega FontAwesome para iconos -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
