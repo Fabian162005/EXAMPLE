e4574fb623fd78a7ea3c075d478fdad4be4de221
Fabian <footballstudios4k@gmail.com>
2025-04-07 13:21:22 -0500

    V1.4.1 CAMBIO DE J-M-F

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,30 @@ Route::get('/', function () {
 
 //RUTA DE LAS ENCUESTAS-----------------------------------------------------------------------------------------------------------------
 });
+Route::get('/encuestas/lima', function () {
+    return view('encuestas.lima');
+});
 
-Route::get('/noticias', function () {
-    return view('layouts.noticias');
-})->name('noticias');
 
+Route::get('/encuestas/chiclayo', function () {
+    return view('encuestas.chiclayo');
+});
+
+Route::get('/encuestas/piura', function () {
+    return view('encuestas.piura');
+});
+Route::get('/encuestas/morropon', function () {
+    return view('encuestas.morropon');
+});
+
+Route::get('/encuestas/castilla', function () {
+    return view('encuestas.castilla');
+});
+
+Route::get('/encuestas/plura2', function () {
+    return view('encuestas.plura2');
+});
+//----------------------------------------------------------------------------------------------------------------------------------------
 Route::get('/volver', function () {
     return view('layouts.app');
 })->name('app');
